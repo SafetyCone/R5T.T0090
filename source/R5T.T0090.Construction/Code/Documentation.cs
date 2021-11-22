3448d6c76695e2179a5c97837f40e8fb62241989
davidcoats <david@getsafetycone.com>
2021-11-21 22:27:03 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0090.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0090.Construction
+{
+    /// <summary>
+    /// Construction project for IJsonKey and IJsonKeyOperator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0090/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0090
+{
+    /// <summary>
+    /// IJsonKey and IJsonKeyOperator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
